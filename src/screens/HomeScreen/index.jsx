e536fb75cfbe81967f2821fbda419bab5ebd344d
Wrokbrook repo <chukschiboy@gmail.com>
2022-01-07 14:01:58 +0100

    creating the home screen for the errand app

---
+++ src/screens/HomeScreen/index.jsx
@@ -0,0 +1,15 @@
+//import libraries
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// create a component
+const HomeScreen = (props) => {
+    return (
+        <View>
+            <Text>MyComponent</Text>
+        </View>
+    );
+};
+
+//make this component available to the app
+export default HomeScreen;
