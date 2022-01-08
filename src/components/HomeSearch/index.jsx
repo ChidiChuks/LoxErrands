8bfff60df8b2fefa6675b5d9e9d6bf0ad2d37657
Wrokbrook repo <chukschiboy@gmail.com>
2022-01-08 06:33:40 +0100

    styling the homescreen

--- src/components/HomeSearch/index.jsx
+++ src/components/HomeSearch/index.jsx
@@ -2,12 +2,26 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+// personal imports
+import styles from '../HomeSearch/styles';
+
 // create a component
 const HomeSearch = () => {
     return (
-        <View style={styles.container}>
-            <Text>HomeSearch</Text>
+        <View>
             {/* Input Box */}
+            <View style={styles.inputBox}>
+                <Text style={styles.inputText}>Where To?</Text>
+
+                <View style={styles.timeContainer}>
+                    <Icon name={"clockcircle"} size={16} color={'#535353'} />
+                    <Text>Now</Text>
+                    <MaterialIcons name={'keyboard-arrow-down'} size={16} />
+                </View>
+            </View>
             {/* Previous Destination */}
             {/* Home Destination */}
         </View>
